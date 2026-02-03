ce0c7d8a092be2b085a472478cf2d48b4a230684
Stefan Fibig <stefan@vr-bits.de>
2026-02-03 12:19:59 +0100

    added to sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,6 +29,7 @@ export default defineUserConfig({
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
       { text: 'API', link: '/api/' },
+      { text: 'Cepheus SRD', link: '/docs/cepheus-srd/' },
     ],
     sidebar: {
       '/guide/': [
@@ -46,6 +47,52 @@ export default defineUserConfig({
           children: [
             '/api/components.md',
           ]
+        },
+      ],
+      '/docs/cepheus-srd/': [
+        {
+          text: 'About',
+          link: '/docs/cepheus-srd/src/about.md',
+        },
+        {
+          text: 'Introduction',
+          link: '/docs/cepheus-srd/src/introduction.md',
+        },
+        {
+          text: 'Book 1: Characters',
+          children: [
+            '/docs/cepheus-srd/src/book1/character-creation.md',
+            '/docs/cepheus-srd/src/book1/skills.md',
+            '/docs/cepheus-srd/src/book1/psionics.md',
+            '/docs/cepheus-srd/src/book1/equipment.md',
+            '/docs/cepheus-srd/src/book1/personal-combat.md',
+          ]
+        },
+        {
+          text: 'Book 2: Starships and Interstellar Travel',
+          children: [
+            '/docs/cepheus-srd/src/book2/off-world-travel.md',
+            '/docs/cepheus-srd/src/book2/trade-and-commerce.md',
+            '/docs/cepheus-srd/src/book2/ship-design-and-construction.md',
+            '/docs/cepheus-srd/src/book2/common-vessels.md',
+            '/docs/cepheus-srd/src/book2/space-combat.md',
+          ]
+        },
+        {
+          text: 'Book 3: Referees',
+          children: [
+            '/docs/cepheus-srd/src/book3/environments-and-hazards.md',
+            '/docs/cepheus-srd/src/book3/worlds.md',
+            '/docs/cepheus-srd/src/book3/planetary-wilderness-encounters.md',
+            '/docs/cepheus-srd/src/book3/social-encounters.md',
+            '/docs/cepheus-srd/src/book3/starship-encounters.md',
+            '/docs/cepheus-srd/src/book3/refereeing-the-game.md',
+            '/docs/cepheus-srd/src/book3/adventures.md',
+          ]
+        },
+        {
+          text: 'Legal',
+          link: '/docs/cepheus-srd/src/legal.md',
         },
       ],
     },
